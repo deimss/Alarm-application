9394c0d7571f394802a233e89af4f18824eea63a
deimss <deimss95@gmail.com>
2017-10-26 16:24:18 +0300

    add v.react+babel

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 import css from './app.scss';
 
-console.log('HELLO from app.js and webpack');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
           publicPath: '/dist'
 
         })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
       }
     ]
   },
